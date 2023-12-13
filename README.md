c60713189ffd7fa1bb9de20c2718538a9eb50263
Sherlocked_hzoi <Sherlocked_hzoi@163.com>
2023-12-13 13:46:00 +0800

    removed param.py
    Former-commit-id: 596d6306488e1ea1ced4f214d8c39aacc1382d3c

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-### machine-learning-nlp
+# machine-learning-nlp
 北京邮电大学机器学习创新实践课程大作业，实现了类 jieba 库的中文分词与标记算法以及 TextRank 关键词提取算法
+### 运行方法
+`python main.py`
+- 数据集是否已分词：`--not_divided`
+- 数据集是否包括标记：`--with_tag`
+- 重新训练：`--retrain`
+- 打印帮助：`-h, --help`
+### 代码结构
+- `data/`：所有的数据集与词典
+- `dic.py`：字典、词典的构造
+- `main.py`：主程序
+- `pred.py`：HMM 模型实现
+- `extractor`：TextRank 算法实现
+- `seg.py`：词典与预测结合的算法模型实现
+- `ds.py`：数据集的构造
+- `code.py`：语句-向量转换工具实现
+- `utils.py`：一些常用工具实现
--- utils.py
+++ utils.py
@@ -1,4 +1,18 @@
 from editdistance import eval as levenshtein_distance
+import inspect
+
+class HyperParam:
+    def __init__(self, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+
+class Base:
+    def save_hyperparameters(self, ignore=[]):
+        frame=inspect.currentframe().f_back
+        _, _, _, values = inspect.getargvalues(frame)
+        self.hparams = {k: v for k, v in values.items() if k not in set(ignore+['self']) and not k.startswith('_')}
+        for k,v in self.hparams.items():
+            setattr(self, k, v)
 
 err=1e-10
 def to_sentence(words):
